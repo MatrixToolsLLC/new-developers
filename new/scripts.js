34357b2faaf81b07bf905c4bf634cd8ad0a1beb3
JBoothUA <jesseboothua@gmail.com>
2018-11-17 23:06:37 -0600

    save point

--- new/scripts.js
+++ new/scripts.js
@@ -1,3 +1,8 @@
 $(function () {
-  alert('Scripts are loaded.');
+  console.log('Scripts are loaded.');
+
+  $('.blue-square').click(function() {
+    alert('blue square was clicked');
+    window.location = 'dog-world.html'
+  });
 })
