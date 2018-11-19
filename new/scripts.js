c9ca0e54ee47f0b844e9b854678b9e83dd22b5a7
EReginaldIII <eddie@matrix.tools>
2018-11-19 04:56:11 -0500

    click

--- new/scripts.js
+++ new/scripts.js
@@ -5,4 +5,16 @@ $(function () {
     alert('blue square was clicked');
     window.location = 'dog-world.html'
   });
+  $('.red-square').click(function() {
+    alert('red square was clicked');
+    window.location = 'mario.html'
+  });
+  $('.green-square').click(function() {
+    alert('green square was clicked');
+    window.location = 'zelda.html'
+  });
+  $('.yellow-square').click(function() {
+    alert('yellow square was clicked');
+    window.location = 'reverse-flash.html'
+  });
 })
