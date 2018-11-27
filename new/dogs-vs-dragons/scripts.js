b456142dd0b3c6655c24365cb40de0232c7d2501
JBoothUA <jesseboothua@gmail.com>
2018-11-27 17:13:45 -0600

    save point

---
+++ new/dogs-vs-dragons/scripts.js
@@ -0,0 +1,3 @@
+$(function () {
+  console.log('Gameboard Scripts are loaded.');
+})
--- new/scripts.js
+++ new/scripts.js
@@ -1,20 +1,13 @@
 $(function () {
-  console.log('Scripts are loaded.');
-
   $('.blue-square').click(function() {
     alert('blue square was clicked');
-    window.location = 'dog-world.html'
+    window.location = './dogs-vs-dragons/gameboard.html'
   });
+
   $('.red-square').click(function() {
     alert('red square was clicked');
-    window.location = 'mario.html'
+    window.location = './mario/mario.html'
   });
-  $('.green-square').click(function() {
-    alert('green square was clicked');
-    window.location = 'zelda.html'
-  });
-  $('.yellow-square').click(function() {
-    alert('yellow square was clicked');
-    window.location = 'reverse-flash.html'
-  });
+
+  console.log('Scripts are loaded.');
 })
